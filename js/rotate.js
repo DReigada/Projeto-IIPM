a045dabd6b0eab29970b0872f42ce8cd8705ecd6
DReigada <daniel.reigada@gmail.com>
2016-04-03 21:38:50 +0100

    Change the rotate function to keep the current rotation (or not)

--- js/rotate.js
+++ js/rotate.js
@@ -1,6 +1,14 @@
-jQuery.fn.rotate = function(degrees) {
+jQuery.fn.rotate = function(degrees, mode) {
+  if (mode && ($(this).css("transform") != 'none')){
+    var tr = $(this).css("transform");
+    var values = tr.split('(')[1],
+    values = values.split(')')[0],
+    values = values.split(',');
+    var angle = Math.round(Math.asin(values[1]) * (180/Math.PI));
+    degrees += angle;
+  }
   $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
                '-moz-transform' : 'rotate('+ degrees +'deg)',
                '-ms-transform' : 'rotate('+ degrees +'deg)',
                'transform' : 'rotate('+ degrees +'deg)'});
-           };
+};
