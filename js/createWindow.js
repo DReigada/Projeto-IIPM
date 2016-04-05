4cb09beb2feca496d678c5a6427d22cb9676c566
DReigada <daniel.reigada@gmail.com>
2016-04-05 21:30:41 +0100

    Style: Windows are now placed near the button that was pressed

--- js/createWindow.js
+++ js/createWindow.js
@@ -1,20 +1,50 @@
 $(".newWindowButton").click(function(event) {
-     var scale = 0.6;
+  var scale = 0.6;
 
-     var newWindow = $($("#windowTemplate").html()).appendTo('#table');
-     newWindow.css('transform', 'scale(' + scale + ')');
-     newWindow.find(".window").css({'transform': 'rotate(0deg)'});
-     var limits = [0,0, $("#table").width() - newWindow.find(".window").width() * scale, $("#table").height() - newWindow.find(".window").height() * scale];
+  var newWindow = $($("#windowTemplate").html()).appendTo('#table'); // create the new window
+  newWindow.css('transform', 'scale(' + scale + ')');                // scale the window
 
-     newWindow.draggable({
-       iframeFix : true,
-       containment: limits
-     });
-     newWindow.find(".exitButton").click(function(event) {
-       $(this).parents(".windowDraggable").remove();
-     });
-     newWindow.find(".rotateButton").click(function(event) {
-       $(this).parents(".window").animate({'transform': '+=rotate(90deg)'});
+  var top, left, angle;
+  switch (event.delegateTarget.id) {
+    case "topButton":
+      top = '15%';
+      left = '18.5%';
+      angle = 180;
+      break;
+    case "bottomButton":
+      top = '56%';
+      left = '18.5%';
+      angle = 0;
+      break;
+    case "leftButton":
+      top = '40%';
+      left = '-10%';
+      angle = 90;
+      break;
+  case "rightButton":
+    top = '40%';
+    left = '52%';
+    angle = -90;
+    break;
+  }
+  newWindow.css({
+    top: top,
+    left: left
+  });
+  newWindow.find(".window").css({'transform': 'rotate('+ angle + 'deg)'});
 
-     });
+
+  var limits = [0,0, $("#table").width() - newWindow.find(".window").width() * scale, $("#table").height() - newWindow.find(".window").height() * scale];
+
+  newWindow.draggable({
+   iframeFix : true,
+   containment: limits
+  });
+  newWindow.find(".exitButton").click(function(event) {
+   $(this).parents(".windowDraggable").remove();
+  });
+  newWindow.find(".rotateButton").click(function(event) {
+   $(this).parents(".window").animate({'transform': '+=rotate(90deg)'});
+
+  });
  });
