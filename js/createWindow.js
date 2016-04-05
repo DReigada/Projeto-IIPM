3226a3ad8f2160ae3f78e69b1b323926843d41da
DReigada <daniel.reigada@gmail.com>
2016-04-05 21:42:43 +0100

    Add comments to clarify the createWindow Script

--- js/createWindow.js
+++ js/createWindow.js
@@ -1,9 +1,12 @@
 $(".newWindowButton").click(function(event) {
   var scale = 0.6;
 
-  var newWindow = $($("#windowTemplate").html()).appendTo('#table'); // create the new window
-  newWindow.css('transform', 'scale(' + scale + ')');                // scale the window
+  // create the new window
+  var newWindow = $($("#windowTemplate").html()).appendTo('#table');
+  // scale the window
+  newWindow.css('transform', 'scale(' + scale + ')');
 
+  // the values that depend on the button pressed
   var top, left, angle;
   switch (event.delegateTarget.id) {
     case "topButton":
@@ -27,19 +30,23 @@ $(".newWindowButton").click(function(event) {
     angle = -90;
     break;
   }
+  // apply the values to the position and rotation
   newWindow.css({
     top: top,
     left: left
   });
   newWindow.find(".window").css({'transform': 'rotate('+ angle + 'deg)'});
 
-
+  // the container to drag the window
   var limits = [0,0, $("#table").width() - newWindow.find(".window").width() * scale, $("#table").height() - newWindow.find(".window").height() * scale];
 
+  // make the window draggable
   newWindow.draggable({
    iframeFix : true,
    containment: limits
   });
+
+  // associate the exit and rotate buttons to their functions
   newWindow.find(".exitButton").click(function(event) {
    $(this).parents(".windowDraggable").remove();
   });
