876d7d2155f124e870856e02c77b87363e1fe5b2
DiMesq <d.mesquita285@gmail.com>
2016-04-19 19:09:41 +0100

    Adds voted music to a user list and decreases the number of votes available.

--- js/music_votes.js
+++ js/music_votes.js
@@ -3,13 +3,26 @@ $(function(){
 	
 	// init storage
 	if (!localStorage.bar) {
-    localStorage.bar = JSON.parse({});
+    localStorage.bar = JSON.stringify({});
 	}
 	if (!sessionStorage.n_votes_left){
 		sessionStorage.n_votes_left = MAX_N_VOTES;
-		sessionStorage.user_votes = JSON.parse({});
+		sessionStorage.user_votes = JSON.stringify({});
 	}
 	
+	$('.music-vote-button').on('click', function(e){
+		// get name
+		music = $(this).closest('li').find('.name').html();
+		
+		// increase the number of votes this music has got
+		voteMusic(music);
+		
+		// add to table of votes or update quantity
+		var musicObj = musicList.get("name", music)[0]._values;		
+		
+		//TODO: addMusicToVotesTable(musicObj.name);
+		
+	})
 	
 })
 
