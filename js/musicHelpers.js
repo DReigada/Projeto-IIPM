be851fafbfdf2d3712630865ccdb6244506145da
DiMesq <d.mesquita285@gmail.com>
2016-04-25 13:12:10 +0100

    Adds a state to the user votes and allows him to delete his previous votes.

--- js/musicHelpers.js
+++ js/musicHelpers.js
@@ -15,26 +15,59 @@ function Music(name, artist, category, duration){ //add year when it was release
 }
 
 function voteMusic(music) {
-	user_votes = JSON.parse(sessionStorage.user_votes);
-	n_votes_left = Number(sessionStorage.n_votes_left);
+	var user_votes = JSON.parse(sessionStorage.user_votes);
+	var n_votes_left = Number(sessionStorage.n_votes_left);
+	var next_id = Number(sessionStorage.next_vote_id);
 
-	if (n_votes_left){
-		user_votes[music] ? user_votes[music] += 1 : user_votes[music] = 1;
-		--n_votes_left;
-	}
+	if (n_votes_left == 0) return;
+	
+	user_votes[next_id] = {'music': music, 'state': IN_10_MIN};
+	--n_votes_left;
 
 	$("#votes-left span").html(n_votes_left);
-	if (n_votes_left == 0) {
-		$('.music-vote-button').disable(true);
-	}
-
+	if (n_votes_left == 0) $('.music-vote-button').disable(true);
+	
+	addMusicToVotesTable(music, next_id);
+	
+	sessionStorage.next_vote_id = next_id + 1;
 	sessionStorage.user_votes = JSON.stringify(user_votes);
 	sessionStorage.n_votes_left = n_votes_left;
 }
 
-function addMusicToVotesTable(music){
-  var votes = JSON.parse(sessionStorage.user_votes);
+function addMusicToVotesTable(music, vote_id){
   var $tableRow = $($("#voteTemplate").html()).appendTo('#votesTableBody');
   $tableRow.find('.name').html(music);
-  $tableRow.find('.votes').html("voted");
+  $tableRow.find('.music-state').html(IN_10_MIN);
+	$tableRow.closest('tr').data('vote-id', vote_id);
 }
+
+function unvoteMusic(row) {
+	var user_votes = JSON.parse(sessionStorage.user_votes);
+	var vote_id = row.data('vote-id');
+	var music_name = user_votes[vote_id].music;
+	var music_state = user_votes[vote_id].state;
+	
+	/* TODO: send message to user if any of the following checks is true */
+	// check if music id is not valid (should never happen)
+	if (vote_id >= sessionStorage.next_vote_id) return;
+	
+	// check if music hasn't played yet and is not the next song playing
+	if (music_state == HAS_PLAYED || music_state == NEXT_SONG) return;
+	/* END TODO */
+	
+	/* ## UPDATE STORAGE ## */
+
+	delete user_votes[vote_id]; // remove vote
+	sessionStorage.user_votes = JSON.stringify(user_votes);
+	var n_votes_left = Number(sessionStorage.n_votes_left) + 1; // increase number of votes left
+	sessionStorage.n_votes_left = n_votes_left;
+	
+	/* ## UPDATE DISPLAY ## */
+	$("#votes-left span").html(n_votes_left); // update votes left display
+	row.remove(); // delete music vote from votes table
+	// allow new votes (only changes something if user had already used up his votes)
+	$('.music-vote-button').disable(false);
+}
+	
+	
+	
--- js/music_votes.js
+++ js/music_votes.js
@@ -1,12 +1,11 @@
 $(function(){
-	const MAX_N_VOTES = 3;
-
 	// init storage
 	if (!localStorage.bar) {
     localStorage.bar = JSON.stringify({});
 	}
 	if (!sessionStorage.n_votes_left){
 		sessionStorage.n_votes_left = MAX_N_VOTES;
+		sessionStorage.next_vote_id = 1;
 		sessionStorage.user_votes = JSON.stringify({});
 	
 	} else { 
@@ -15,28 +14,27 @@ $(function(){
 		if (n_votes_left == 0) $('.music-vote-button').disable(true);
 	}
 
-
-
 	var votes = JSON.parse(sessionStorage.user_votes);
 
-	for(music in votes){
+	for(vote in votes){
 		var $tableRow = $($("#voteTemplate").html()).appendTo('#votesTableBody');
-		$tableRow.find('.name').html(music);
-		$tableRow.find('.votes').html("voted");
+		$tableRow.find('.name').html(votes[vote].music);
+		$tableRow.find('.music-state').html(votes[vote].state);
+		$tableRow.data('vote-id', vote);
 	}
 
 	$('.music-vote-button').on('click', function(e){
 		// get name
 		music = $(this).closest('li').find('.name').html();
-
-		// increase the number of votes this music has got
+	
+		// adds vote
 		voteMusic(music);
 
-		// add to table of votes or update quantity
-		var musicObj = musicList.get("name", music)[0]._values;
-
-		addMusicToVotesTable(musicObj.name);
-
+	})
+	
+	$('#votesTable').on('click', '.music-unvote-button', function(e){
+		var $row = $(this).closest('tr');
+		unvoteMusic($row);
 	})
 
 })
